1b1547d0660cd32332f4ae4716268679d4ab4552
= <=>
2025-01-22 23:42:08 +0530

    Add Home Page

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains : ['www.w3schools.com' , "img.daisyui.com"]
   },
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,26 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <div className="area">
+    <ul className="circles">
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+    <div className="flex h-screen justify-center flex-col items-center">
+         <h1 className="  text-lg">Hello world!</h1>
+         <h1 className=" text-white italic underline "><Link href="/shop">Go to Shop &#8594; </Link></h1>
+      </div>
+  </div>
+      
    
   );
 }
