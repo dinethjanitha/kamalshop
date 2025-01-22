bcc5d960add23b908c2e1641faa7aed73e0286d0
= <=>
2025-01-22 23:27:32 +0530

    Configure Next.js for static exports and enable React strict mode

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains : ['www.w3schools.com' , "img.daisyui.com"]
-  }
+  },
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
